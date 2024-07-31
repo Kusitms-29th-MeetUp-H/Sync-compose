07863469b3de5a87d32f04aca3bbcd6f9e540212
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-07-31 17:42:44 +0900

    [feat] 2주차 바텀바 실습

--- app/src/main/java/com/example/sync_compose/MainActivity.kt
+++ app/src/main/java/com/example/sync_compose/MainActivity.kt
@@ -3,17 +3,25 @@ package com.example.sync_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -55,26 +63,47 @@ fun BottomNavigationBar(navController: NavHostController) {
         BottomNavItem("chatting", "Chatting", R.drawable.ic_bnv_chat),
         BottomNavItem("my", "My", R.drawable.ic_bnv_my)
     )
-    BottomNavigation {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach { item ->
-            BottomNavigationItem(
-                icon = { Icon(painterResource(id = item.icon), contentDescription = item.label) },
-                label = { Text(item.label) },
-                selected = currentRoute == item.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            saveState = true
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+            .background(Color.Transparent)
+            .border(1.dp, color = Color.Cyan, RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+    ) {
+        BottomNavigation(
+            modifier = Modifier.height(78.dp),
+            backgroundColor = Color.Black,
+            elevation = 20.dp
+        ) {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+            items.forEach { item ->
+                BottomNavigationItem(
+                    modifier = Modifier.padding(vertical = 16.dp),
+                    icon = {
+                        Icon(
+                            painterResource(id = item.icon),
+                            contentDescription = item.label
+                        )
+                    },
+                    label = { Text(item.label) },
+                    selected = currentRoute == item.route,
+                    selectedContentColor = Color.Blue,
+                    unselectedContentColor = Color.Red,
+                    onClick = {
+                        navController.navigate(item.route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
                     }
-                }
-            )
+                )
+            }
         }
     }
+
 }
 
 data class BottomNavItem(val route: String, val label: String, val icon: Int)
